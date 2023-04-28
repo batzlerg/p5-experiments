a8c864c2fb557b95efc5763dad2fdc513a2af93d
Graham Batzler <graham.batzler@hearst.com>
2023-04-27 23:10:04 -0600

    refine hangman UI and fix guessed word list

--- src/hangman/sketch.js
+++ src/hangman/sketch.js
@@ -1,17 +1,26 @@
-const MAX_WRONG_GUESSES = 6;
+const textSizes = {
+  MEDIUM: 32,
+  LARGE: 64,
+}
+const MAX_WRONG_GUESSES = 5; // todo: based on level
+let gameState = {
+  guessedLetters: [],
+  wrongGuesses: [],
+  gameOver: false,
+  word: "",
+  wordState: []
+};
 
-let gameState = {};
 let words = [];
-
-let randomIndex = 0;
-let randomWord = null;
+let currentWord = null;
 
 function preload() {
-  words = loadJSONC("words.jsonc", function(data) {
+  words = loadJSONC("words.jsonc", function (data) {
     words = data;
-    randomIndex = floor(random(words.length));
-    randomWord = words[randomIndex];
-    initializeGameState(gameState);
+    const currentWordIndex = floor(random(words.length));
+    currentWord = words[currentWordIndex];
+    console.log(`WORD IS "${currentWord}"`);
+    initializeGameState();
   });
 }
 
@@ -26,27 +35,21 @@ function draw() {
     push();
     background(220);
     drawGallows();
-    drawHangman(gameState.wrongGuesses);
-    drawWordState(gameState.wordState);
-    drawGuessedLetters(gameState);
-    checkGameOver(gameState);
+    drawHangman();
+    drawWordState();
+    drawGuessedLetters();
+    drawGameOver();
     pop();
   } else {
     push();
     background(220);
     textAlign(CENTER);
-    textSize(32);
-    text("Loading" + ellipses, width/2, height/2);
+    textSize(textSizes.MEDIUM);
+    text("Loading" + ellipses, width / 2, height / 2);
     pop();
-    
+
     if (frameCount % 30 === 0) { // change ellipses every 30 frames
-      if (ellipses === "...") {
-        ellipses = "..";
-      } else if (ellipses === "..") {
-        ellipses = ".";
-      } else {
-        ellipses = "...";
-      }
+      ellipses = (ellipses === "...") ? ".." : (ellipses === "..") ? "." : "...";
     }
   }
 }
@@ -58,128 +61,139 @@ function drawGallows() {
   line(100, 100, 100, height - 50);
   line(100, 100, 300, 100);
   line(300, 100, 300, 150);
+  line(150, height - 50, 250, height - 50); // add a horizontal line
   pop();
 }
 
-function drawHangman(wrongGuesses) {
+function drawHangman() {
   push();
   stroke(0);
   strokeWeight(4);
-  // Head
-  if (wrongGuesses >= 1) ellipse(300, 200, 80, 80);
-  // Body
-  if (wrongGuesses >= 2) line(300, 240, 300, 400);
-  // Left arm
-  if (wrongGuesses >= 3) line(300, 280, 240, 350);
-  // Right arm
-  if (wrongGuesses >= 4) line(300, 280, 360, 350);
-  // Left leg
-  if (wrongGuesses >= 5) line(300, 400, 240, 480);
-  // Right leg
-  if (wrongGuesses >= 6) line(300, 400, 360, 480);
+  const numWrongGuesses = gameState.wrongGuesses.length
+
+  if (numWrongGuesses >= 1) {
+    // Head
+    ellipse(300, 200, 80, 80);
+  }
+  if (numWrongGuesses >= 2) {
+    // Body
+    line(300, 240, 300, 400);
+  }
+  if (numWrongGuesses >= 3) {
+    // Left arm
+    line(300, 280, 240, 350);
+  }
+  if (numWrongGuesses >= 4) {
+    // Right arm
+    line(300, 280, 360, 350);
+  }
+  if (numWrongGuesses >= 5) {
+    // Left leg
+    line(300, 400, 240, 480);
+  }
+  if (numWrongGuesses >= 6) {
+    // Right leg
+    line(300, 400, 360, 480);
+  }
   pop();
 }
 
-function drawWordState(wordState){
+function drawWordState() {
   push();
   textSize(48);
   textAlign(RIGHT, BOTTOM);
   fill(0);
-  const wordStateString = wordState.join(" ");
+  const wordStateString = gameState.wordState.join(" ");
   text(wordStateString, width - 20, height - 20);
   pop();
 }
 
-function drawGuessedLetters(){
-  push();
-  textSize(24);
-  textAlign(LEFT, TOP);
-  fill(0);
-  text("Guessed letters:", 20, 20);
-  const guessedLettersString = gameState.guessedLetters.join(" ");
-  text(guessedLettersString, 20, 50);
-  pop();
-}
-
-function checkGameOver(){
-  push();
-  if (gameState.wordState === gameState.word) {
-    gameState.isGameOver = true;
-    fill(0);
-    textSize(48);
-    textAlign(CENTER, CENTER);
-    const message = "You win!";
-    const w = textWidth(message) + 20;
-    const h = 80;
-    rectMode(CENTER);
-    fill(255);
-    rect(width / 2, height / 2 - 40, w, h);
-    fill(0);
-    text(message, width / 2, height / 2 - 40);
-    textSize(24);
-    text("Click to play again", width / 2, height / 2 + 20);
-  } else if (gameState.wrongGuesses >= MAX_WRONG_GUESSES) {
-    gameState.isGameOver = true;
-    fill(0);
-    textSize(48);
-    textAlign(CENTER, CENTER);
-    const message = "Game over";
-    const w = textWidth(message) + 20;
-    const h = 120;
-    rectMode(CENTER);
-    fill(255);
-    rect(width / 2, height / 2 - 40, w, h);
-    fill(0);
-    text(message, width / 2, height / 2 - 60);
-    textSize(24);
-    text(`The word was "${gameState.word}". Click to play again`, width / 2, height / 2 + 20);
+function drawGuessedLetters() {
+  let x = 25;
+  let y = 50;
+  let letter;
+  for (let i = 0; i < gameState.guessedLetters.length; i++) {
+    letter = gameState.guessedLetters[i];
+    if (gameState.word.includes(letter)) {
+      fill(0);
+    } else {
+      stroke(150);
+      fill(255);
+    }
+    textSize(textSizes.MEDIUM);
+    text(letter, x, y);
+    x += 40;
   }
-  pop();
-}
-
-function mouseClicked() {
-  if (gameState.isGameOver) {
-    initializeGameState(gameState);
-  }
-}
-
-function initializeGameState(){
-  gameState.word = getRandomWord();
-  gameState.wordState = gameState.word.split("").map(() => "_");
-  gameState.wrongGuesses = 0;
-  gameState.guessedLetters = [];
-  gameState.isGameOver = false;
 }
 
 function keyPressed() {
-  const letter = key.toUpperCase();
-  if (!gameState.isGameOver && isValidGuess(letter)) {
-    if (gameState.word.includes(letter)) {
-      updateWordState(gameState, letter);
+  if (gameState.gameOver) return;
+  if (keyCode >= 65 && keyCode <= 90) { // A-Z
+    const letter = String.fromCharCode(keyCode);
+    const alreadyGuessed = gameState.guessedLetters.includes(letter);
+    if (alreadyGuessed) {
+      return false;
+      // todo: glow letter?
     } else {
-      gameState.wrongGuesses++;
+      updateGuessedLetter(letter)
+      updateWordState(letter);
     }
-    gameState.guessedLetters.push(letter);
   }
 }
 
-function updateWordState(state, letter) {
+function updateGuessedLetter(letter) {
+  gameState.guessedLetters.push(letter);
+}
+
+function updateWordState(letter) {
+  let isLetterFound = false;
   for (let i = 0; i < gameState.word.length; i++) {
     if (gameState.word[i] === letter) {
       gameState.wordState[i] = letter;
+      isLetterFound = true;
     }
+  }
+  if (!isLetterFound) {
+    gameState.wrongGuesses.push(letter);
   }
 }
 
-function isValidGuess(letter) {
-  const validLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return (
-    validLetters.includes(letter) &&
-    !gameState.guessedLetters.includes(letter)
-  );
+function drawGameOver() {
+  const isLose = gameState.wrongGuesses.length >= MAX_WRONG_GUESSES;
+  const isWin = !gameState.wordState.includes("_");
+  const isEndState = isLose || isWin;
+
+  if (isEndState) {
+    gameState.gameOver = true;
+    const gameOverText =
+      isWin ? 'You Win!' :
+      isLose ? 'Game Over' :
+      'Something Went Wrong';
+
+    push();
+    textAlign(CENTER);
+    textSize(textSizes.LARGE);
+    fill(isLose ? 255 : 0, isWin ? 167 : 0, 0);
+    text(
+      gameOverText,
+      width / 2,
+      height / 2
+    );
+    pop();
+  }
 }
 
-function getRandomWord() {
-  const randomIndex = Math.floor(random(0, words.length + 1));
-  return words[randomIndex];
+function initializeGameState() {
+  gameState.word = currentWord.toUpperCase();
+  gameState.wordState = Array(gameState.word.length).fill("_");
+}
+
+function resetGameState() {
+
+  initializeGameState();
+}
+
+function _setRandomWord() {
+  const currentWordIndex = floor(random(words.length));
+  currentWord = words[currentWordIndex];
 }
